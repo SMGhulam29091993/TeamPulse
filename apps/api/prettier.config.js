abef3e6e6d4c3078cbec898425ab1c4b244add9f
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-05-10 16:19:25 +0530

    fix: added type import

--- apps/api/prettier.config.js
+++ apps/api/prettier.config.js
@@ -1,4 +1,4 @@
-import indexConfig from '@repo/config-prettier/index.json';
+import indexConfig from '@repo/config-prettier/index.json' with {type : "json"};
 
 export default {
     ...indexConfig,
