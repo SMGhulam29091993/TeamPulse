82b6a9f7dc79a01caf7231ae4066cadea67b1278
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-05-10 22:47:59 +0530

    fix: the .env file load as per node_env

--- apps/api/src/config/env-config.ts
+++ apps/api/src/config/env-config.ts
@@ -1,18 +1,12 @@
 import dotenv from 'dotenv';
 
-switch(process.env.NODE_ENV === 'development'){
-    //use .env.development for development environment
-    case true:
-        dotenv.config({ path: '.env.development' });
-        break;
-    //use .env for production environment
-    default:
-        dotenv.config({ path: '.env' });
-        break;
-}
+const envFile =
+    process.env.NODE_ENV === 'production' ? '.env' : '.env.development';
+dotenv.config({ path: envFile });
 
-export const config =  {
-    port : Number.isFinite(Number(process.env.PORT)) ? Number(process.env.PORT) :  3000,
-    host : process.env.HOST as string ?? 'localhost',
-}
-
+export const config = {
+    port: Number.isFinite(Number(process.env.PORT))
+        ? Number(process.env.PORT)
+        : 3000,
+    host: (process.env.HOST as string) ?? 'localhost',
+};
