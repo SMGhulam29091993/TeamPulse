0950b3bed0ad0e4fd2d39a49e90c45ee781d67ff
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-05-10 22:12:58 +0530

    fix: add new dev script and removed dotenv from server

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,18 +1,15 @@
-import app from "./app";
 import "colors";
-import dotenv from 'dotenv';
+import app from "./app";
 import { config } from "./config/env-config";
-
-dotenv.config();
 
 const PORT = config.port;
 const HOST = config.host;
 
 const server = app.listen(Number(PORT), HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`.bgGreen);
+  console.log(`Server is running on http://${HOST}:${PORT}`.bgGreen);
 });
 
-server.on('error', (err) => {
-    console.error('Error starting server:'.bgRed, err);
-    process.exit(1);
+server.on("error", (err) => {
+  console.error("Error starting server:".bgRed, err);
+  process.exit(1);
 });
