ad9d0820a2bf0a0eee8adafef043dfa4c9e42d3c
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-05-17 17:51:23 +0530

    feat: initialized prisma connection to db on server start

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,32 +1,37 @@
-import express, {Request, Response, type Express} from 'express';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Request, Response, type Express } from 'express';
+import prisma from './database/prisma.client';
 
-const app : Express = express();
+const app: Express = express();
 
+// Use the imported Prisma client instance instead of constructing a new one.
+prisma;
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174']
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
 
 const corsOptions = {
-    origin : (origin : string | undefined, callback: (err : Error | null, allow? : boolean)=>void)=>{
+    origin: (
+        origin: string | undefined,
+        callback: (err: Error | null, allow?: boolean) => void
+    ) => {
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials : true,
-}
+    credentials: true,
+};
 
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (_req : Request, res : Response) => {
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World!');
 });
-
 
 export default app;
