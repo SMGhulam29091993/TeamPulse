b49a52b5d74249d7b9d0622a7866a5854d2fca16
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-06-07 22:03:21 +0530

    fix: updated the unused T from Icontroller

--- apps/api/src/shared/interfaces/IController.ts
+++ apps/api/src/shared/interfaces/IController.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
-export interface IController<T> {
-    getAll(req: Request, res: Response): Promise<void>;
-    getById(req: Request, res: Response): Promise<void>;
-    create(req: Request, res: Response): Promise<void>;
-    update(req: Request, res: Response): Promise<void>;
-    delete(req: Request, res: Response): Promise<void>;
+import { NextFunction, Request, Response } from 'express';
+export interface IController {
+    getAll(req: Request, res: Response, next: NextFunction): Promise<void>;
+    getById(req: Request, res: Response, next: NextFunction): Promise<void>;
+    create(req: Request, res: Response, next: NextFunction): Promise<void>;
+    update(req: Request, res: Response, next: NextFunction): Promise<void>;
+    delete(req: Request, res: Response, next: NextFunction): Promise<void>;
 }
