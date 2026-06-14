cba3eab04d3b5ab1ac146d185b46be094435b436
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-06-14 20:59:56 +0530

    fix: updated the express user request field

--- apps/api/src/types/express.d.ts
+++ apps/api/src/types/express.d.ts
@@ -1,11 +1,10 @@
 import 'express';
-import { Role } from '@prisma/client';
 
 declare module 'express-serve-static-core' {
     export interface Request {
         user?: {
             id: string;
-            role: Role;
+            jti: string;
         };
     }
 }
