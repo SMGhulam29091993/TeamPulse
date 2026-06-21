f3d8f12f347aeea60e91b6e86e4e745c0942d24b
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2026-06-22 00:32:05 +0530

    feat: added authcontroller

---
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -0,0 +1,71 @@
+import { NextFunction, Request, Response } from 'express';
+import { cookieOptions } from '../../shared/lib/constants';
+import { successResponse } from '../../shared/utils/response.util';
+import { IAuthService } from './auth.interface';
+import {
+    LoginDto,
+    LoginResponseDto,
+    RegisterDto,
+    VerifyOtpDto,
+    VerifyOtpResponseDto,
+} from './auth.types';
+
+export class AuthController {
+    constructor(private readonly authService: IAuthService) {}
+
+    register = async (req: Request, res: Response, _next: NextFunction) => {
+        const { firstName, lastName, email, username, password } = req.body;
+        const payload: RegisterDto = {
+            firstName,
+            lastName,
+            username,
+            email,
+            password,
+        };
+        const registerResponse = await this.authService.register(payload);
+        return successResponse(
+            res,
+            201,
+            'Registration successful',
+            registerResponse
+        );
+    };
+
+    verifyEmail = async (req: Request, res: Response, _next: NextFunction) => {
+        const { otp, identifier } = req.body;
+
+        const payload: VerifyOtpDto = { otp, identifier };
+        const verifyOtpResponse = await this.authService.verifyEmail(payload);
+        const response: Partial<VerifyOtpResponseDto> = {
+            username: verifyOtpResponse.username,
+            email: verifyOtpResponse.email,
+            accessToken: verifyOtpResponse.accessToken,
+        };
+        res.cookie(
+            'refreshToken',
+            verifyOtpResponse.refreshToken,
+            cookieOptions
+        );
+        return successResponse(
+            res,
+            200,
+            'Email verified successfully',
+            response
+        );
+    };
+
+    login = async (req: Request, res: Response, _next: NextFunction) => {
+        const { email, password } = req.body;
+        const payload: LoginDto = {
+            email,
+            password,
+        };
+
+        const { accessToken, refreshToken }: LoginResponseDto =
+            await this.authService.login(payload);
+
+        res.cookie('refreshToken', refreshToken, cookieOptions);
+
+        return successResponse(res, 200, 'Login Successful.', { accessToken });
+    };
+}
